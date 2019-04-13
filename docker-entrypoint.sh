23cb54224c59aa6f413869bc964709fc28aab4a2
GΛVĪN <gavin@scurr.me>
2019-04-13 11:06:47 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export AMBER='\033[0;33m'
+export BLUE='\033[0;34m'
+export NC='\033[0m'
+
+export CONFIG='/usr/local/etc/unbound'
+
+mountpoint -q ${CONFIG} && printf "${GREEN}[INFO] ${CONFIG} is mounted on container${NC}\n" || printf "${RED}[WARN] ${CONFIG} is not mounted on container${NC}\n"
 rm -f "/var/run/unbound/*.pid"
 
 exec "$@"
